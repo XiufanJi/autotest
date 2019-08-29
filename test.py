c52c80525ee6f0a29b73062e6483e0a5933b260a
Xiufanji <timegao@outlook.com>
2019-08-29 09:48:40 +0800

    update files

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
+#斐波那契数列输出测试
 def fibonacci(n):
     x, y,count= 0,1,0
     while count<n:
-        print(x, end=" ")
+        print(x,end=" ")
         #x,y=y,x+y这种方式，等价于先进行等式右边的计算，然后再将结果赋值给等式的左边
         x, y = y, x+y
         count+=1
