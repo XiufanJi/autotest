0ca59dbb66b417642bf7a9749527227995c26781
Xiufanji <timegao@outlook.com>
2019-08-29 09:45:12 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    x, y,count= 0,1,0
+    while count<n:
+        print(x, end=" ")
+        #x,y=y,x+y这种方式，等价于先进行等式右边的计算，然后再将结果赋值给等式的左边
+        x, y = y, x+y
+        count+=1
+
+if __name__ == '__main__':
+    fibonacci(10)
+
+
+
+
